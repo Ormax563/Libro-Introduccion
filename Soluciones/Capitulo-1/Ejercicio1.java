920a94324413b16c243702425e626129ac9ccce9
Jesús Ormaza <46572423+Ormax563@users.noreply.github.com>
2020-04-16 12:53:34 -0500

    Add files via upload

---
+++ Soluciones/Capitulo-1/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("***************************************************");
+		System.out.println("***************************************************");
+		System.out.println("****           Me llamo Jesús Ormaza           ****");
+		System.out.println("****       y estoy aprendiendo a programar     ****");
+		System.out.println("***************************************************");
+		System.out.println("***************************************************");
+
+	}
+
+}
