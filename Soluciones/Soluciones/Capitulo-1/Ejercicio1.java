f685ad13baf157f00755dfc3883e1757c286cce6
Jesús Ormaza <46572423+Ormax563@users.noreply.github.com>
2020-04-17 13:36:03 -0500

    Add files via upload

---
+++ Soluciones/Soluciones/Capitulo-1/Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("***************************************************");
+		System.out.println("***************************************************");
+		System.out.println("****           Jesús David Ormaza Espín        ****");
+		System.out.println("***************************************************");
+		System.out.println("***************************************************");
+
+	}
+
+}
